297a89c398b325fa5be6ca86ab61ff969584fdc0
Jon <jharder@kipsu.com>
2023-08-12 17:31:15 -0500

    Impliment minimal GET and SET

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+
+struct Memory {
+    storage: HashMap<String, String>,
+}
+
+impl Memory {
+    fn new() -> Self {
+        Memory {
+            storage: HashMap::new(),
+        }
+    }
+
+    fn get(&self, key: &String) -> Option<&String> {
+        self.storage.get(key)
+    }
+
+    fn set(&mut self, key: String, val: String) {
+        self.storage.insert(key, val);
+    }
+}
 
 fn main() {
     let host_port = "127.0.0.1:6379";
     let listener = TcpListener::bind(host_port).unwrap();
+    let mut storage = Memory::new();
+    storage.set("foo".to_string(), "bar".to_string());
     println!("Server started at {host_port}");
     for stream in listener.incoming() {
         match stream {
-            Ok(mut s) => {
+            Ok(mut stream) => {
                 let mut buf = vec![0; 512];
-                let bytes_read = s.read(&mut buf).unwrap();
+                let bytes_read = stream.read(&mut buf).unwrap();
                 if bytes_read == 0 {
                     break;
                 }
                 let message = String::from_utf8(buf).unwrap();
                 let commands = parse_message(message);
                 for command in commands {
-                    handle_command(command, &mut s);
+                    handle_command(command, &mut storage, &mut stream);
                 }
             }
             Err(err) => {
@@ -31,6 +54,8 @@ fn main() {
 enum Keyword {
     Ping,
     Echo,
+    Get,
+    Set,
     Unknown,
 }
 
@@ -40,7 +65,7 @@ struct Command {
     args: Vec<String>,
 }
 
-fn handle_command(command: Command, stream: &mut TcpStream) {
+fn handle_command(command: Command, memory: &mut Memory, stream: &mut TcpStream) {
     let result = match command.keyword {
         Keyword::Ping => stream.write("+PONG\r\n".as_bytes()),
         Keyword::Echo => {
@@ -48,7 +73,25 @@ fn handle_command(command: Command, stream: &mut TcpStream) {
             let len = response.len();
             stream.write(format!("${len}\r\n{response}\r\n").as_bytes())
         }
+        Keyword::Get => {
+            if let [key, ..] = command.args.as_slice() {
+                match memory.get(key) {
+                    Some(val) => stream.write(format!("${}\r\n{}\r\n", val.len(), val).as_bytes()),
+                    None => stream.write("$-1\r\n".as_bytes()),
+                }
+            } else {
+                stream.write("-Missing argument to GET command\r\n".as_bytes())
+            }
+        }
         Keyword::Unknown => stream.write("-Unknown command\r\n".as_bytes()),
+        Keyword::Set => {
+            if let [key, val, ..] = command.args.as_slice() {
+                memory.set(key.to_owned(), val.to_owned());
+                stream.write("+OK\r\n".as_bytes())
+            } else {
+                stream.write("-Not enough arguments to SET command.\r\n".as_bytes())
+            }
+        }
     };
     println!("RESULT: {result:?}");
 }
@@ -89,6 +132,8 @@ fn parse_message(message: String) -> Vec<Command> {
                     keyword = match word.to_uppercase().as_str() {
                         "PING" => Keyword::Ping,
                         "ECHO" => Keyword::Echo,
+                        "GET" => Keyword::Get,
+                        "SET" => Keyword::Set,
                         _ => Keyword::Unknown,
                     };
                     matched_keyword = true;
